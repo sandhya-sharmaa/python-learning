9ba97d17d25b498656c529e6a090e0f628dd385a
sandhya-sharmaa <sandhhya07@gmail.com>
2026-07-09 00:13:23 +0545

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,52 @@
+# DATA TYPE
+
+# name ="sandhya sharma"
+# age = 21
+# height =5.3
+# is_student = True
+# print (name)
+# print (age)
+# print(is_student)
+# print(type(name))
+# print(type(age))
+# print(type(is_student))
+
+
+# TYPE 2 TYPE CONVERSION
+# age ="21"
+# age =int(age)
+# print(type(age))
+
+# number =10
+# print(float(number))
+
+
+
+# OPERATORS
+# a= 20
+# b= 5
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a%b)
+# print(a**b)
+# print(a//b)
+
+
+
+
+
+# name = input ("enter your name:")
+# age = int(input("enter your age:"))
+
+# print("Name:",name)
+# print("Age:",age)
+
+
+a=20
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
