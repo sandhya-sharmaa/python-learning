9c495a8f9808c015c26ab33a4dfb67f8b903f4f3
sandhya-sharmaa <sandhhya07@gmail.com>
2026-07-09 23:42:01 +0545

    Add files via upload
    Day 3: Learned Python Boolean Values, Comparison Operators, if...else, Nested if nd Logical Operators.

--- lesson3.py
+++ lesson3.py
@@ -1,4 +1,39 @@
-age = 20
+# boolean
+# age = 21 
+# if age >=18:
+#     print("you can vote")
 
-if age >= 18:
-    print("You are an adult.")
+
+# example 1:
+# is_student=True
+# print(is_student)
+
+# if Statement
+
+# if age >=18:   
+#  print("adult")
+
+# mark=35
+# if mark>=40:
+#     print("pass")
+#     print("congratulation")
+    
+# print("Better luck next time")
+
+
+# else condition
+
+# EXAMPLE 1
+
+# age =21 
+# if age>=18:
+#     print("you can vote")
+# else :
+#     print("you cannot vote")
+
+# EXAMPLE 2
+age = 15
+if age >brea=18:
+    print("you can vote ")
+else:
+    print("you cannot vote")
